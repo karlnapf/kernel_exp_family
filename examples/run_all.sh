8fa17456449ff50852af81fc18c3a551d1af36c2
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-10-22 10:33:00 +0100

    test

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -1,1 +1,1 @@
-find examples/ -type f -name '*.py' ! -name '__init__.py' | xargs -i sh -c 'echo Running {}; python {} > /dev/null'
+find examples/ -type f -name '*.py' ! -name '__init__.py' | xargs python
