55438584bd797eaabc2ca41dc4da71af916644f5
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-10-22 10:49:16 +0100

    another debug msg

--- examples/run_all.sh
+++ examples/run_all.sh
@@ -1,3 +1,4 @@
 export PYTHONPATH=$PYTHONPATH:.
+echo $PYTHONPATH
 python -c 'import kernel_exp_family'
 find examples/ -type f -name '*.py' ! -name '__init__.py' | xargs -i sh -c 'echo Running {}; python {}'
