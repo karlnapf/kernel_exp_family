38ac7adab1dc222e56c12df3e01369b9c7d91bfa
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-10-22 14:54:41 +0100

    initial commit

---
+++ examples/demo_xvalidation_manual.py
@@ -0,0 +1,53 @@
+from kernel_exp_family.estimators.lite.gaussian import KernelExpLiteGaussian
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+if __name__ == '__main__':
+    """
+    This simple demo demonstrates how to select the kernel parameter for the lite
+    estimator, based on a simple manual grid search, using the in-built x-validation.
+    As the xvalidation is implemented in the API base class, this can be easily
+    changed to other parameters or estimators.
+    """
+    N = 200
+    D = 2
+    
+    # fit model to samples from a standard Gaussian
+    X = np.random.randn(N, D)
+    
+    
+    # create grid over sigma parameters, fixed regulariser
+    log_sigmas = np.linspace(-5, 10, 20)
+    lmbda = 1.
+    
+    # evaluate objective function over all those parameters
+    O = np.zeros(len(log_sigmas))
+    O_lower = np.zeros(len(log_sigmas))
+    O_upper = np.zeros(len(log_sigmas))
+    
+    # grid search
+    for i, sigma in enumerate(log_sigmas):
+        est = KernelExpLiteGaussian(np.exp(sigma), lmbda, D)
+        
+        # this is an array num_repetitions x num_folds, each containing a objective
+        xval_result = est.xvalidate_objective(X, num_folds=5, num_repetitions=2)
+        O[i] = np.mean(xval_result)
+        O_lower[i] = np.percentile(xval_result, 10)
+        O_upper[i] = np.percentile(xval_result, 90)
+    
+    # best parameter
+    best_log_sigma = log_sigmas[np.argmin(O)]
+    
+    # visualisation
+    plt.plot([best_log_sigma, best_log_sigma], [np.min(O), np.max(O)], 'r')
+    plt.plot(log_sigmas, O, 'b-')
+    plt.plot(log_sigmas, O_lower, 'b--')
+    plt.plot(log_sigmas, O_upper, 'b--')
+    plt.xlim([np.min(log_sigmas) - 1, np.max(log_sigmas) + 1])
+    plt.xlabel("log sigma")
+    plt.ylabel("objective")
+    plt.title("lmbda=%.4f" % lmbda)
+    plt.legend(["Best sigma", "Performance"])
+    plt.legend(["Best sigma", "Performance", "80% percentile"])
+    plt.show()
