a2d256433a2f58b454f395f8dcfa8a4884734718
Heiko Strathmann <heiko.strathmann@gmail.com>
2015-11-07 18:54:58 +0000

    added hot-started bayesian optimisation initialisation via using posterior mean as prior mean

--- kernel_exp_family/estimators/parameter_search_bo.py
+++ kernel_exp_family/estimators/parameter_search_bo.py
@@ -1,22 +1,90 @@
+from pybo import inits, recommenders, solvers, policies
+import reggie
+from reggie.means._core import Mean
+
 import numpy as np
-from pybo import solve_bayesopt
+
+
+class GPMean(Mean):
+    """
+    Allows prescribing a posterior GP mean as the prior mean for
+    another GP.
+    
+    Taken from dev-branch of reggie
+    """
+    def __init__(self, gp):
+        super(GPMean, self).__init__()
+        self._gp = gp.copy()
+
+    def __info__(self):
+        info = []
+        info.append(('gp', self._gp))
+        return info
+
+    def get_mean(self, X):
+        mu, _ = self._gp.predict(X)
+        return mu
+
+    def get_grad(self, X):
+        return iter([])
+
+    def get_gradx(self, X):
+        _, _, dmu, _ = self._gp.predict(X, grad=True)
+        return dmu
 
 
 class BayesOptSearch(object):
-    def __init__(self, estimator, data, param_bounds, objective_log=False, objective_log_bound=1000):
+    def __init__(self, estimator, data, param_bounds, objective_log=False,
+                 objective_log_bound=1000, n_initial=6):
         self.estimator = estimator
         self.data = data
         self.param_bounds = param_bounds
         self.objective_log = objective_log
         self.log_bound = objective_log_bound
-#     
-#         # parameter space dimensions correspond to sorted parameter bound keys
+        self.n_initial = n_initial
+
+        # parameter space dimensions correspond to sorted parameter bound keys
         self.param_names = np.sort(param_bounds.keys())
         self.bounds = np.array([param_bounds[k] for k in self.param_names])
-         
-        self.xbest = None
-        self.model = None
-        self.info = None
+        
+        self.initialised = False
+
+    def _init_model(self, n_initial, previous_model=None):
+        # get initial data and some test points.
+        self.X = list(inits.init_latin(self.bounds, n_initial))
+        self.Y = [self._eval(x) for x in self.X]
+        
+        # initial values for kernel parameters, taken from pybo code
+        sn2 = 1e-6
+        rho = np.max(self.Y) - np.min(self.Y)
+        rho = 1. if (rho < 1e-1) else rho
+        ell = 0.25 * (self.bounds[:, 1] - self.bounds[:, 0])
+        
+        if previous_model is None:
+            # use data mean as GP mean
+            bias = np.mean(self.Y)
+            self.model = reggie.make_gp(sn2, rho, ell, bias)
+            
+            # define priors if gp was created from scratch
+            self.model.params['mean.bias'].set_prior('normal', bias, rho)
+            self.model.params['like.sn2'].set_prior('horseshoe', 0.1)
+            self.model.params['kern.rho'].set_prior('lognormal', np.log(rho), 1.)
+            self.model.params['kern.ell'].set_prior('uniform', ell / 100, ell * 10)
+        else:
+            # if there has been a previous model, use it as mean
+            like = previous_model._like
+            kern = previous_model._kern
+            mean = GPMean(previous_model)
+            self.model = reggie.GP(like, kern, mean)
+        
+        # initialize the MCMC inference meta-model and add data
+        self.model.add_data(self.X, self.Y)
+        self.model = reggie.MCMC(self.model, n=10, burn=100)
+        
+        # best point so far
+        self.xbest = recommenders.best_incumbent(self.model, self.bounds, self.X)
+        
+        self.initialised = True
 
     def _search_domain_to_param_dict(self, x):
         # build parameter dictionary for estimator, change from log to std space
@@ -31,11 +99,39 @@ class BayesOptSearch(object):
         return param_dict
     
     def optimize(self, num_iter=10):
-        self.xbest, self.model, self.info = solve_bayesopt(self._eval, self.bounds,
-                                                   niter=num_iter, model=self.model)
+        if not self.initialised:
+            self._init_model(n_initial=self.n_initial)
+        
+        for _ in range(num_iter):
+            index = policies.EI(self.model, self.bounds, self.X)
+            xnext, _ = solvers.solve_lbfgs(index, self.bounds)
+
+            # observe and update model
+            ynext = self._eval(xnext)
+            self.model.add_data(xnext, ynext)
+            self.X.append(xnext)
+            self.Y.append(ynext)
+
+            # best point so far
+            self.xbest = recommenders.best_incumbent(self.model, self.bounds, self.X)
         
         return self._search_domain_to_param_dict(self.xbest)
 
+    def re_initialise(self, new_data=None, n_initial=1):
+        if not self.initialised:
+            raise RuntimeError("Model needs to be optimised before re-initialisation is possible. Call optimise() method")
+        
+        if new_data is not None:
+            self.data = new_data
+        
+        # sample a random posterior model
+        previous_model = self.model._models[np.random.randint(self.model._n)]
+        
+        # use as prior mean
+        self._init_model(n_initial=n_initial, previous_model=previous_model)
+        
+        return self._search_domain_to_param_dict(self.xbest)
+            
     def _eval(self, x):
         param_dict = self._search_domain_to_param_dict(x)
         
--- kernel_exp_family/examples/demo_xvalidation_bayes_opt.py
+++ kernel_exp_family/examples/demo_xvalidation_bayes_opt.py
@@ -5,31 +5,25 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-if __name__ == '__main__':
-    """
-    This simple demo demonstrates how to select the kernel parameter for the lite
-    estimator, based on a Bayesian optimisation black-box optimiser.
-    """
-    N = 200
-    D = 2
-    
-    # fit model to samples from a standard Gaussian
-    X = np.random.randn(N, D)
-    data = np.random.randn(200, D)
-    est = KernelExpLiteGaussian(sigma=1, lmbda=.001, D=D)
-    
-    # specify bounds of parameters to search for
-    param_bounds = {
-#               'lmbda': [-3,-1], # fixed lmbda, uncomment to include in search
-              'sigma': [-3,8],
-              }
-    
-    # oop interface for optimising and using results
-    # objective is not put through log here, if it is, might want to bound away from zero
-    bo = BayesOptSearch(est, data, param_bounds, objective_log=False, objective_log_bound=100)
-    best_params =  bo.optimize(num_iter=10)
-    est.set_parameters_from_dict(best_params)
-    
+def visualise_objective():
+    # plot cost function learned through Bayesian optimisation, if 1D
+    if len(param_bounds) == 1:
+        bounds = param_bounds[param_bounds.keys()[0]]
+        x = np.linspace(bounds[0], bounds[1], 500)
+        mu, s2 = bo.model.predict(x[:, None])
+        
+        plt.figure()
+        plt.plot(x, mu, 'b-')
+        lower = mu - 1.96 * np.sqrt(s2)
+        upper = mu + 1.96 * np.sqrt(s2)
+        plt.plot(x, lower, 'b--')
+        plt.plot(x, upper, 'b--')
+        plt.plot(np.ravel(bo.X), bo.Y, 'rx')
+        plt.plot([bo.xbest, bo.xbest], [lower, upper], 'r-')
+        plt.title("objective model")
+        plt.grid(True)
+
+def visualise_fit():
     # visualise found fit
     plt.figure()
     Xs = np.linspace(-5, 5)
@@ -44,26 +38,60 @@ if __name__ == '__main__':
     visualise_array(Xs, Ys, G, X)
     plt.title("estimate gradient norm")
     
-    plt.suptitle("Best fit %s\nOptimised over: %s" % 
-                 (str(est.get_parameters()), str(param_bounds)))
     plt.tight_layout()
     
+
+
+if __name__ == '__main__':
+    """
+    This simple demo demonstrates how to select the kernel parameter for the lite
+    estimator, based on a Bayesian optimisation black-box optimiser.
+    Note that this optimiser can be "hot-started", i.e. it can be reset, but using
+    the previous model as initialiser for the new optimisation, which is useful
+    when the objective function changes slightly.
+    """
+    N = 200
+    D = 2
     
-    # plot cost function learned through Bayesian optimisation, if 1D
-    if len(param_bounds) == 1:
-        bounds = param_bounds[param_bounds.keys()[0]]
-        x = np.linspace(bounds[0], bounds[1], 500)
-        mu, s2 = bo.model.predict(x[:, None])
-        
-        plt.figure()
-        plt.plot(x, mu, 'b-')
-        lower = mu-1.96*np.sqrt(s2)
-        upper = mu+1.96*np.sqrt(s2)
-        plt.plot(x, lower, 'b--')
-        plt.plot(x, upper, 'b--')
-        plt.plot(np.ravel(bo.info.x), bo.info.y, 'rx')
-        plt.plot([bo.xbest, bo.xbest], [lower, upper], 'r-')
-        plt.title("objective model")
-        plt.grid(True)
+    # fit model to samples from a standard Gaussian
+    X = np.random.randn(N, D)
+    est = KernelExpLiteGaussian(sigma=1, lmbda=.1, D=D)
+    
+    # specify bounds of parameters to search for
+    param_bounds = {
+#               'lmbda': [-3,-1], # fixed lmbda, uncomment to include in search
+              'sigma': [-3, 4],
+              }
+    
+    # oop interface for optimising and using results
+    # objective is not put through log here, if it is, might want to bound away from zero
+    bo = BayesOptSearch(est, X, param_bounds, objective_log=False, objective_log_bound=100,
+                        n_initial=5)
+    
+    # optimisation starts here, use results and apply to model
+    best_params = bo.optimize(num_iter=5)
+    est.set_parameters_from_dict(best_params)
+    
+    visualise_fit()
+    plt.suptitle("Original fit %s\nOptimised over: %s" % 
+             (str(est.get_parameters()), str(param_bounds)))
+    visualise_objective()
+    plt.suptitle("Objective")
+    
+    # now change data, with different length scale
+    X = np.random.randn(200, D) * .1
+    
+    # reset optimiser, which but initialise from old model, sample 3 random point to update
+    best_params = bo.re_initialise(new_data=X, n_initial=3)
+    
+    # this optimisation now runs on the "new" objective
+    best_params = bo.optimize(num_iter=3)
+    est.set_parameters_from_dict(best_params)
+    
+    visualise_fit()
+    plt.suptitle("New fit %s\nOptimised over: %s" % 
+             (str(est.get_parameters()), str(param_bounds)))
+    visualise_objective()
+    plt.suptitle("New objective")
     
     plt.show()
