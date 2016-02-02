fix computation of new log weights

kernel_exp_family/estimators/finite/gaussian.py
@@ -193,10 +193,7 @@ class KernelExpFiniteGaussian(EstimatorBase):
         
         # update terms and weights
         self.n += len(X)
-        new_weights = np.zeros(N + 1)
-        new_weights[0] = self.log_sum_weights
-        new_weights[1:] = log_weights
-        self.log_sum_weights = log_sum_exp(new_weights)
+        self.log_sum_weights = log_sum_exp(list(log_weights) + [self.log_sum_weights])
         
         # finally update solution
         self.theta = fit_L_C_precomputed(self.b, self.L_C)