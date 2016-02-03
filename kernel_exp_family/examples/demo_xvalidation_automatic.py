443c0e235b8697fb9828afbbb9df441218bdc48b
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-02-03 16:33:36 +0000

    fix example after name change of parameter

--- kernel_exp_family/examples/demo_xvalidation_automatic.py
+++ kernel_exp_family/examples/demo_xvalidation_automatic.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     # fully automatic parameter tuning in every fit call
     est = KernelExpLiteGaussianAdaptive(sigma=1., lmbda=.001, D=D, N=N,
                                         # these paramters are all optional, to controll Bayesian opt.
-                                        n_initial=3, n_iter=3, minimum_size_learning=100,
+                                        num_initial_evaluations=3, n_iter=3, minimum_size_learning=100,
                                         # these depend on how much data changes between the "fit" calls
                                         n_initial_relearn=3, n_iter_relearn=3,
                                         # this can be used to adjust search spaces or include more parameters
