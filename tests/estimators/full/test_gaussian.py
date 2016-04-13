b7c4a87ff4cabc5c49f24e9ede875d00b037c29b
Esben Sorig <esben@sorig.eu>
2016-04-13 17:56:55 +0100

    Use proper build system, remove autograd dependency

--- tests/estimators/full/test_gaussian.py
+++ tests/estimators/full/test_gaussian.py
@@ -1,8 +1,7 @@
-from autograd import hessian, grad
 from nose.tools import assert_almost_equal
 from numpy.ma.testutils import assert_close
+import numpy as np
 
-import autograd.numpy as np  # Thinly-wrapped numpy
 from kernel_exp_family.estimators.full.develop.gaussian import compute_lower_right_submatrix_loop, \
     compute_RHS_loop, log_pdf_naive, build_system_fast, build_system_loop
 from kernel_exp_family.estimators.full.gaussian import SE_dx_i_dx_j, \
@@ -25,59 +24,6 @@ def setup():
     data = np.random.multivariate_normal(mean, cov, size=N)
 
     return data, l, sigma, lmbda
-
-
-def test_SE_dx_i_dx_j():
-    d = 3
-    l = 2.0
-
-    x = np.random.randn(d).reshape(-1, 1)
-    y = np.random.randn(d).reshape(-1, 1)
-
-    left_arg_hessian = SE_dx_i_dx_j(x, y, l)
-    autograd_left_arg_hessian = hessian(lambda a: SE(a, y, l))
-    autograd_computation = np.squeeze(autograd_left_arg_hessian(x))
-
-    assert_close(left_arg_hessian, autograd_computation)
-
-
-def test_SE_dx_i_dx_i_dx_j():
-    d = 3
-    l = 2.0
-
-    x = np.random.randn(d).reshape(-1, 1)
-    y = np.random.randn(d).reshape(-1, 1)
-
-    derivative_matrix = SE_dx_i_dx_i_dx_j(x, y, l)
-    autograd_derivative_matrix = hessian(lambda a: SE_dx(a, y, l))
-    autograd_computation = np.squeeze(autograd_derivative_matrix(x))
-
-    d = x.shape[0]
-    autograd_result = np.zeros((d, d))
-
-    for i in range(d):
-        autograd_result[i, :] = autograd_computation[i, i, :]
-
-    assert_close(derivative_matrix, autograd_result)
-
-def test_grad():
-    sigma = 1.
-    lmbda = 1.
-    N = 10
-    D = 2
-    est = KernelExpFullGaussian(sigma, lmbda, D, N)
-
-    X = np.random.randn(N, D)
-    est.fit(X)
-
-    auto_gradient = grad(est.log_pdf)
-
-    x_new = np.random.randn(D)
-
-    # print(est.grad(x_new))
-    # print(auto_gradient(x_new))
-
-    assert_close(est.grad(x_new), auto_gradient(x_new))
 
 
 def test_build_system_partially_vectorised_equals_implementation():
