9bce08cc3b7320fee80d5f3e37a1e9b13003b951
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-04-14 15:00:06 +0100

    add naive gradient method to full and nystrom, with tests

--- kernel_exp_family/estimators/full/gaussian_nystrom.py
+++ kernel_exp_family/estimators/full/gaussian_nystrom.py
@@ -1,8 +1,9 @@
 from kernel_exp_family.estimators.estimator_oop import EstimatorBase
 from kernel_exp_family.estimators.full.develop.gaussian_nystrom import log_pdf_naive,\
-    nystrom_naive
+    nystrom_naive, grad_naive
 from kernel_exp_family.tools.assertions import assert_array_shape
 import numpy as np
+
 
 def nystrom(X, sigma, lmbda, inds):
     A, b = build_system(X, sigma, lmbda)
@@ -17,7 +18,6 @@ def nystrom(X, sigma, lmbda, inds):
     return A_mm, A_nm, b_m, inds
 
 def fit(X, sigma, lmbda, inds):
-    N,D = X.shape
     A_nm, b = nystrom(X, sigma, lmbda, inds)
     
     A = np.dot(A_nm.T, A_nm)
@@ -27,6 +27,13 @@ def fit(X, sigma, lmbda, inds):
     alpha = x[0]
     beta = x[1:]
     return alpha, beta
+
+def log_pdf(x, X, sigma, alpha, beta, inds):
+    return log_pdf_naive(x, X, sigma, alpha, beta, inds)
+
+def grad(x, X, sigma, alpha, beta, inds):
+    return grad_naive(x, X, sigma, alpha, beta, inds)
+    
 
 class KernelExpFullNystromGaussian(EstimatorBase):
     def __init__(self, sigma, lmbda, D, N, m):
@@ -41,6 +48,7 @@ class KernelExpFullNystromGaussian(EstimatorBase):
         self.X = np.zeros((0, D))
         
         self.inds = np.sort(np.random.permutation(N*D)[:m])
+        self.m = m
     
     def fit(self, X):
         assert_array_shape(X, ndim=2, dims={0: self.N, 1: self.D})
@@ -48,11 +56,11 @@ class KernelExpFullNystromGaussian(EstimatorBase):
         self.alpha, self.beta = fit(self.X, self.sigma, self.lmbda, self.inds)
     
     def log_pdf(self, x):
-        return log_pdf_naive(x, self.X, self.sigma, self.alpha, self.beta, self.inds)
+        return log_pdf(x, self.X, self.sigma, self.alpha, self.beta, self.inds)
 
     def grad(self, x):
         assert_array_shape(x, ndim=1, dims={0: self.D})
-        return np.zeros(self.D)
+        return grad(x, self.X, self.sigma, self.alpha, self.beta, self.inds)
 
     def log_pdf_multiple(self, X):
         return np.array([self.log_pdf(x) for x in X])
--- tests/estimators/full/test_gaussian_nystrom.py
+++ tests/estimators/full/test_gaussian_nystrom.py
@@ -2,7 +2,8 @@ from nose.tools import assert_almost_equal
 from numpy.testing.utils import assert_allclose
 
 import kernel_exp_family.estimators.full.develop.gaussian as gaussian_full_develop
-from kernel_exp_family.estimators.full.develop.gaussian_nystrom import log_pdf_naive
+from kernel_exp_family.estimators.full.develop.gaussian_nystrom import log_pdf_naive,\
+    grad_naive
 from kernel_exp_family.estimators.full.gaussian import KernelExpFullGaussian
 import kernel_exp_family.estimators.full.gaussian as gaussian_full
 from kernel_exp_family.estimators.full.gaussian_nystrom import KernelExpFullNystromGaussian,\
@@ -54,7 +55,7 @@ def test_fit_nystrom_all_inds_equals_fit():
     alpha, beta = fit(X, sigma, lmbda, inds)
     
     # low accuracy as numerically potentially unstable
-    assert_almost_equal(alpha_full, alpha, delta=1e-1)
+    assert_allclose(alpha_full, alpha, rtol=1e-1)
     assert_allclose(beta_full.reshape(N*D,), beta, rtol=1e-1)
 
 def test_full_pipeline_nystrom_all_inds_equals_full():
@@ -64,12 +65,44 @@ def test_full_pipeline_nystrom_all_inds_equals_full():
     D = 2
     
     X = np.random.randn(N,D)
-    x = np.zeros(D)
+    X2 = np.random.randn(N,D)
     
     est = KernelExpFullNystromGaussian(sigma, lmbda, D, N, m=N*D)
     est.fit(X)
     est2 = KernelExpFullGaussian(sigma, lmbda, D, N)
     est2.fit(X)
     
-    # low accuracy as numerically potentially unstable
-    assert_allclose(est.log_pdf(x), est2.log_pdf(x), rtol=1e-2)
+    for x in X2:
+        # low accuracy as numerically potentially unstable
+        assert_allclose(est.log_pdf(x), est2.log_pdf(x), rtol=1e-2)
+
+def test_grad_naive_nystrom_all_inds_equals_grad_naive_full_alpha_equals_0():
+    N = 10
+    D = 2
+    X = np.random.randn(N, D)
+    sigma = 1.
+    alpha = 0.
+    beta = np.random.randn(N, D)
+    beta_nystrom = beta.flatten()
+    inds = np.arange(N * D)
+    
+    for x in np.random.randn(N,D):
+        a = gaussian_full_develop.grad_naive(x, X, sigma, alpha, beta)
+        b = grad_naive(x, X, sigma, alpha, beta_nystrom, inds)
+        assert_allclose(a, b)
+        
+        
+def test_grad_naive_nystrom_all_inds_equals_grad_naive_full():
+    N = 10
+    D = 2
+    X = np.random.randn(N, D)
+    sigma = 1.
+    alpha = np.random.randn()
+    beta = np.random.randn(N, D)
+    beta_nystrom = beta.flatten()
+    inds = np.arange(N * D)
+    
+    for x in np.random.randn(N,D):
+        a = gaussian_full_develop.grad_naive(x, X, sigma, alpha, beta)
+        b = grad_naive(x, X, sigma, alpha, beta_nystrom, inds)
+        assert_allclose(a, b)
