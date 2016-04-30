b5282b00bc995e7434ab431eea2925f8f434c273
Heiko Strathmann <heiko.strathmann@gmail.com>
2016-04-30 09:05:53 +0100

    added a "flat" version of build_system_nystrom

--- kernel_exp_family/estimators/full/develop/gaussian_nystrom.py
+++ kernel_exp_family/estimators/full/develop/gaussian_nystrom.py
@@ -1,9 +1,42 @@
 from kernel_exp_family.estimators.full.gaussian import build_system, compute_h, \
     compute_xi_norm_2, compute_first_row, compute_RHS
+from kernel_exp_family.estimators.full.gaussian_nystrom import compute_lower_right_submatrix_component,\
+    compute_first_row_without_storing
 from kernel_exp_family.kernels.kernels import gaussian_kernel_dx_dx, \
     gaussian_kernel_grad, gaussian_kernel_dx_i_dx_i_dx_j, \
     gaussian_kernel_dx_i_dx_j, gaussian_kernel_hessians
 import numpy as np
+
+def ind_to_ai(ind, D):
+    """
+    For a given row index of the A matrix, return corresponding data and component index
+    """
+    return ind / D, ind % D
+
+def build_system_nystrom_modular_slow(X, sigma, lmbda, inds):
+    N, D = X.shape
+    m = len(inds)
+
+    h = compute_h(X, sigma).reshape(-1)
+    xi_norm_2 = compute_xi_norm_2(X, sigma)
+    
+    A_mn = np.zeros((m + 1, N * D + 1))
+    A_mn[0, 0] = np.dot(h, h) / N + lmbda * xi_norm_2
+    
+    for row_idx in range(len(inds)):
+        for col_idx in range(N * D):
+            A_mn[1 + row_idx, 1 + col_idx] = compute_lower_right_submatrix_component(X, lmbda, inds[row_idx], col_idx, sigma)
+    
+    ## compute in parallel with joblib
+    #results = Parallel(n_jobs=4)(delayed(compute_lower_right_submatrix_component)(X, lmbda, inds[row_idx], col_idx, sigma) for (row_idx, col_idx) in row_col_ind_pairs)
+    
+    
+    A_mn[0, 1:] = compute_first_row_without_storing(X, h, N, lmbda, sigma)
+    A_mn[1:, 0] = A_mn[0, inds + 1]
+    
+    b = compute_RHS(h, xi_norm_2)
+    
+    return A_mn, b
 
 def build_system_nystrom_naive_from_all_hessians(X, sigma, lmbda, inds):
     n, d = X.shape
@@ -36,12 +69,6 @@ def build_system_nystrom_naive_from_full(X, sigma, lmbda, inds):
     A_nm = A[:, inds_with_xi]
     
     return A_nm, b
-
-def ind_to_ai(ind, D):
-    """
-    For a given row index of the A matrix, return corresponding data and component index
-    """
-    return ind / D, ind % D
 
 def log_pdf_naive(x, X, sigma, alpha, beta, inds):
     N, D = X.shape
